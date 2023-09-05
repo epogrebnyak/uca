9c47e693f0323b5dbe0df94f57bc56f93faa0b2a
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-09-05 06:34:47 +0000

    new skillset dataset

--- example.py
+++ example.py
@@ -1,5 +1,19 @@
 from uca.main import root_node
 
+# SkillSet has a dict of sections
+
+# skillset = SkillSet(title="Skillset Example 2")
+# skillset.sections["PE"] = Section(title="Personal and emotional skills")
+# skillset.sections["PE"].add_document("""
+#Positive attitudes:
+#- Optimism ~ Positive outlook and hopeful attitude towards the future.
+#- Self-efficacy ~ Belief in own's accomplishment.
+#
+#Emotional regulation:
+#- Stress resistance ~ Capability to handle pressure and adversity.
+#- Emotional control
+#""")
+
 skillset = root_node("Skillset Example")
 skillset.add_document(
     """
